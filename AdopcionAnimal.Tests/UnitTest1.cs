e09ead91abbeb815f9ad2a1f1391bc6ec9c05646
Alex-20KD <gonzaloyzam.2018@gmail.com>
2025-05-31 10:15:00 -0500

    commit Implement JWT security, unit tests, and AI sentiment analysis for adoption follow-ups

---
+++ AdopcionAnimal.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace AdopcionAnimal.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3a7f3bec2c584c3e7c50bfe326fd5819435258")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6992b35bb35bd394a079e4e6fc934a89282c2a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
